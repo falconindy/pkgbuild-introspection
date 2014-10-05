7d052ca2b4ac34c2ba5e96ed8b4137c08b74fe4b
Dave Reisner <dreisner@archlinux.org>
2014-10-05 11:07:01 -0400

    add a smoketest-style unit test based on the gcc package

--- test/testcases.py
+++ test/testcases.py
@@ -209,6 +209,124 @@ class TestPkgbuildToAurinfo(unittest.TestCase):
         self.assertEqual(['friendship', 'magic'], pb['ponies']['depends'])
         self.assertNotIn('depends_x86_64', pb['ponies'])
 
+    def test_CoverageSmokeTest(self):
+        pb = testutil.parse_pkgbuild('''
+            pkgname=('gcc' 'gcc-libs' 'gcc-fortran' 'gcc-objc' 'gcc-ada' 'gcc-go')
+            pkgver=4.9.1
+            _pkgver=4.9
+            pkgrel=2
+            _snapshot=4.9-20140903
+            pkgdesc="The GNU Compiler Collection"
+            arch=('i686' 'x86_64')
+            license=('GPL' 'LGPL' 'FDL' 'custom')
+            url="http://gcc.gnu.org"
+            makedepends=('binutils>=2.24' 'libmpc' 'cloog' 'gcc-ada' 'doxygen')
+            checkdepends=('dejagnu' 'inetutils')
+            options=('!emptydirs')
+            source=(ftp://gcc.gnu.org/pub/gcc/snapshots/${_snapshot}/gcc-${_snapshot}.tar.bz2
+                    gcc-4.8-filename-output.patch
+                    gcc-4.9-isl-0.13-hack.patch)
+            md5sums=('24dfd67139fda4746d2deff18182611d'
+                     '40cb437805e2f7a006aa0d0c3098ab0f'
+                     'f26ae06b9cbc8abe86f5ee4dc5737da8')
+
+            package_gcc-libs() {
+              pkgdesc="Runtime libraries shipped by GCC"
+              groups=('base')
+              depends=('glibc>=2.20')
+              options=('!emptydirs' '!strip')
+              install=gcc-libs.install
+            }
+
+            package_gcc() {
+              pkgdesc="The GNU Compiler Collection - C and C++ frontends"
+              depends=("gcc-libs=$pkgver-$pkgrel" 'binutils>=2.24' 'libmpc' 'cloog')
+              groups=('base-devel')
+              options=('staticlibs')
+              install=gcc.install
+            }
+
+            package_gcc-fortran() {
+              pkgdesc="Fortran front-end for GCC"
+              depends=("gcc=$pkgver-$pkgrel")
+              options=('staticlibs' '!emptydirs')
+              install=gcc-fortran.install
+            }
+
+            package_gcc-objc() {
+              pkgdesc="Objective-C front-end for GCC"
+              depends=("gcc=$pkgver-$pkgrel")
+            }
+
+            package_gcc-ada() {
+              pkgdesc="Ada front-end for GCC (GNAT)"
+              depends=("gcc=$pkgver-$pkgrel")
+              options=('staticlibs' '!emptydirs')
+              install=gcc-ada.install
+            }
+
+            package_gcc-go() {
+              pkgdesc="Go front-end for GCC"
+              depends=("gcc=$pkgver-$pkgrel")
+              options=('staticlibs' '!emptydirs')
+              install=gcc-go.install
+            }
+        ''')
+
+        expected_packages = ['gcc', 'gcc-libs', 'gcc-fortran', 'gcc-objc', 'gcc-ada', 'gcc-go']
+        self.assertPackageNamesEqual(pb, expected_packages)
+
+        for pkgname in expected_packages:
+            pkg = pb[pkgname]
+            self.assertEqual('4.9.1', pkg['pkgver'])
+            self.assertEqual('2', pkg['pkgrel'])
+            self.assertEqual(['i686', 'x86_64'], pkg['arch'])
+            self.assertEqual(['GPL', 'LGPL', 'FDL', 'custom'], pkg['license'])
+            self.assertEqual('http://gcc.gnu.org', pkg['url'])
+            self.assertEqual(['binutils>=2.24','libmpc', 'cloog', 'gcc-ada', 'doxygen'], pkg['makedepends'])
+            self.assertEqual(['dejagnu', 'inetutils'], pkg['checkdepends'])
+            self.assertEqual(['ftp://gcc.gnu.org/pub/gcc/snapshots/4.9-20140903/gcc-4.9-20140903.tar.bz2',
+                              'gcc-4.8-filename-output.patch',
+                              'gcc-4.9-isl-0.13-hack.patch'], pkg['source'])
+            self.assertEqual(['24dfd67139fda4746d2deff18182611d',
+                              '40cb437805e2f7a006aa0d0c3098ab0f',
+                              'f26ae06b9cbc8abe86f5ee4dc5737da8'], pkg['md5sums'])
+
+        self.assertEqual('Runtime libraries shipped by GCC', pb['gcc-libs']['pkgdesc'])
+        self.assertEqual(['base'], pb['gcc-libs']['groups'])
+        self.assertEqual(['glibc>=2.20'], pb['gcc-libs']['depends'])
+        self.assertEqual(['!emptydirs', '!strip'], pb['gcc-libs']['options'])
+        self.assertEqual('gcc-libs.install', pb['gcc-libs']['install'])
+
+        self.assertEqual('The GNU Compiler Collection - C and C++ frontends', pb['gcc']['pkgdesc'])
+        self.assertEqual(['gcc-libs=4.9.1-2', 'binutils>=2.24', 'libmpc', 'cloog'], pb['gcc']['depends'])
+        self.assertEqual(['base-devel'], pb['gcc']['groups'])
+        self.assertEqual(['staticlibs'], pb['gcc']['options'])
+        self.assertEqual('gcc.install', pb['gcc']['install'])
+
+        self.assertEqual('Fortran front-end for GCC', pb['gcc-fortran']['pkgdesc'])
+        self.assertEqual(['gcc=4.9.1-2'], pb['gcc-fortran']['depends'])
+        self.assertEqual(['staticlibs', '!emptydirs'], pb['gcc-fortran']['options'])
+        self.assertEqual('gcc-fortran.install', pb['gcc-fortran']['install'])
+        self.assertNotIn('groups', pb['gcc-fortran'])
+
+        self.assertEqual('Objective-C front-end for GCC', pb['gcc-objc']['pkgdesc'])
+        self.assertEqual(['gcc=4.9.1-2'], pb['gcc-objc']['depends'])
+        self.assertEqual(['!emptydirs'], pb['gcc-objc']['options'])
+        self.assertNotIn('install', pb['gcc-objc'])
+        self.assertNotIn('groups', pb['gcc-objc'])
+
+        self.assertEqual('Ada front-end for GCC (GNAT)', pb['gcc-ada']['pkgdesc'])
+        self.assertEqual(['gcc=4.9.1-2'], pb['gcc-ada']['depends'])
+        self.assertEqual(['staticlibs', '!emptydirs'], pb['gcc-ada']['options'])
+        self.assertEqual('gcc-ada.install', pb['gcc-ada']['install'])
+        self.assertNotIn('groups', pb['gcc-ada'])
+
+        self.assertEqual('Go front-end for GCC', pb['gcc-go']['pkgdesc'])
+        self.assertEqual(['gcc=4.9.1-2'], pb['gcc-go']['depends'])
+        self.assertEqual(['staticlibs', '!emptydirs'], pb['gcc-go']['options'])
+        self.assertEqual('gcc-go.install', pb['gcc-go']['install'])
+        self.assertNotIn('groups', pb['gcc-go'])
 
 if __name__ == '__main__':
     unittest.main()
